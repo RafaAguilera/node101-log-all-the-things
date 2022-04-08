8ae08ade96ec701f9dc7f098050ee7a68329c73f
unknown <aguilera.morales.rafa@gmail.com>
2022-04-08 16:34:37 -0700

    complete

--- server/app.js
+++ server/app.js
@@ -1,20 +1,25 @@
-const express = require('express');
-const fs = require('fs');
-const app = express();
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const rt = require("file-stream-rotator")
+let csvToJson = require('convert-csv-to-json');
+let jsonlog = require('../jlog.json')
+let fileInputName = 'log.csv'; 
+let fileOutputName = 'jlog.json';
+app.set("json spaces", 2)
+morgan.token("custom", ":user-agent; :date[iso]; :method; :url; :http-version; :status")
+let writer = rt.getStream({filename:"log.csv"});
+csvToJson.generateJsonFileFromCsv(fileInputName,fileOutputName);
+let json = csvToJson.getJsonFromCsv("log.csv");
 
-app.use((req, res, next) => {
-// write your logging code here
-
-});
-
-app.get('/', (req, res) => {
-// write your code to respond "ok" here
-
-});
-
-app.get('/logs', (req, res) => {
-// write your code to return a json object containing the log data here
-
-});
-
+app.use(morgan("custom", { stream: writer }))
+app.get('/', function (req, res) {
+  let dt = new Date;
+  res.send('Ok!').status(200)
+  console.log(`${req.get('user-agent')},${dt.toISOString()},GET,/,HTTP/1.1,200`)
+  })
+app.get('/logs', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.json(jsonlog);
+    })
 module.exports = app;
